203633aff3b39609fa264524520cda6e5baff678
Jesxen <163159525+Jesxen@users.noreply.github.com>
2026-02-27 20:04:43 +0100

    Update CalculadoraEjemplo.java

--- src/CalculadoraEjemplo.java
+++ src/CalculadoraEjemplo.java
@@ -13,7 +13,6 @@ public class CalculadoraEjemplo {
     private double operando2;
     private double resultado;
 
-    // Constructor
     public CalculadoraEjemplo(double operando1, double operando2) {
         this.operando1 = operando1;
         this.operando2 = operando2;
